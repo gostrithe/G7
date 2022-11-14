6c16a4b555a6871c6cc9f8e1f4475137136a839b
infinitelv <2734723199@qq.com>
2022-11-14 17:28:07 +0800

    dayfour-简单构建分类

--- mock/mockData.js
+++ mock/mockData.js
@@ -3,7 +3,7 @@ const novelListData = require('./data/novelListData.json');
 const novelContentData = require('./data/novelContentData.json');
 // const homeBannerData = require('./data/homeBannerData.json');
 const myHomeData = require('./data/myHomeData.json');
-// const assortImgData=require('./data/assortImgData.json')
+const assortImgData=require('./data/assortImgData.json')
 module.exports = () => {
     return {
         novelDetailData,
--- src/component/user/CategoryThemePage.vue
+++ src/component/user/CategoryThemePage.vue
@@ -1,15 +1,30 @@
 <template>
-    <div>
-        11111
-    </div>
+    <ul class="clearfix">
+        <li :style="{backgroundImage: backgroundimage}"  class="catalog-item"  v-for="n in 23">{{ n }}</li>
+    </ul>
+
 </template>
 
 <script>
 export default {
-
+    data() {
+        return {
+            backgroundimage: `url(https://static.mkzcdn.com/mobile/img/catalogs/ticai/pic_class_img${1}@2x.png)`
+        }
+    }
 }
 </script>
 
 <style lang="scss" scoped>
+.clearfix {
+    .catalog-item {
+        display: inline-block;
+        width: 50vw;
+        height: 30vw;
+        opacity: 1;
+        background-size: contain;
 
+
+    }
+}
 </style>
