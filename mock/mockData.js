ee7d30e6f188455a0d6336edd339f34fcb638f9a
infinitelv <2734723199@qq.com>
2022-11-14 11:42:44 +0800

    dayfour-second

--- mock/mockData.js
+++ mock/mockData.js
@@ -2,14 +2,15 @@ const novelDetailData = require('./data/novelDetailData.json');
 const novelListData = require('./data/novelListData.json');
 const novelContentData = require('./data/novelContentData.json');
 // const homeBannerData = require('./data/homeBannerData.json');
-const myHomeData = require('./data/myHomeData.json')
-
+const myHomeData = require('./data/myHomeData.json');
+// const assortImgData=require('./data/assortImgData.json')
 module.exports = () => {
     return {
         novelDetailData,
         novelListData,
         novelContentData,
         // homeBannerData
-        myHomeData
+        myHomeData,
+        // assortImgData
     }
 }
